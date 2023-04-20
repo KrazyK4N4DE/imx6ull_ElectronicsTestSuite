3edade2affd85b6d390e81f56bab8d9474c4b0a7
WeiWei <327176352@qq.com>
2023-04-20 23:05:52 +0800

    编写输入系统的头文件
    抽象出两个结构体：
    第一个表示输入事件，可以触摸屏输入、网络输入等等。
    第二个表示输入设备。

---
+++ include/input_manager.h
@@ -0,0 +1,27 @@
+#ifndef __INPUT_MANAGER_H__
+#define __INPUT_MANAGER_H__
+
+#define INPUT_TYPE_TOUCH 1  // 触摸屏事件
+#define INPUT_TYPE_NET 2  // 网络事件
+
+// 抽象出输入系统的结构体，可以触摸屏输入、网络输入等等
+typedef struct InputEvent
+{
+	struct timeval tTime;  // 表示时间
+	int iType;  // 输入类型
+	int iX;  // 坐标X
+	int iY;  // 坐标Y
+	int iPressure;  // 按压压力
+	char str[1024];  // 对于网络事件，传输字符串来输入
+}InputEvent, * PInputEvent;
+// 抽象出第二个结构体，表示输入设备
+typedef struct InputDevice
+{
+	char* name;  // 名称
+	int (*GetInputEvent)(PInputEvent ptInputEvent);
+	int (*DeviceInit)();
+	int (*DeviceExit)();
+	struct InputDevice* ptNext;
+}InputDevice, * PInputDevice;
+
+#endif
